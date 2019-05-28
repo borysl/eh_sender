ec9729ceeeff5be773c004c0bb0cac56f8b7a8fa
Schmidt <S27855@upr.u-ssi.net>
2019-05-28 10:28:05 +0200

    Play with consumers

--- send.js
+++ send.js
@@ -4,10 +4,10 @@ const { EventHubClient } = require('@azure/event-hubs');
 const { connectionString, eventHubsName } = require('./config.json');
 
 async function main() {
-  const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName);
+  const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName, { consumerGroup: 'einstein'} );
 
   for (let i = 0; i < 100; i++) {
-    const eventData = {body: `Event ${i}`};
+    const eventData = { body: `Event ${i}` };
     console.log(`Sending message: ${eventData.body}`);
     await client.send(eventData);
   }
