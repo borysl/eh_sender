a74d8eade141d83c1f1c639f4e4a5f7988c75327
Schmidt <S27855@upr.u-ssi.net>
2019-05-28 09:46:29 +0200

    send from the example

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+      "es6": true,
+      "node": true
+    },
+    "parserOptions": {
+      "ecmaVersion": 2017,
+      "sourceType": "module",
+    },
+    "extends": [
+      "eslint:recommended"
+    ],
+    "rules": {
+      "indent": [
+        "error",
+        2,
+        {
+          "SwitchCase" : 1
+        }
+      ],
+      "linebreak-style": [
+        "error",
+        "windows"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ],
+      "no-console": 0
+    }
+  };
---
+++ send.js
@@ -0,0 +1,20 @@
+const { EventHubClient } = require('@azure/event-hubs');
+
+// Define connection string and the name of the Event Hub
+const { connectionString, eventHubsName } = require('./config.json');
+
+async function main() {
+  const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName);
+
+  for (let i = 0; i < 100; i++) {
+    const eventData = {body: `Event ${i}`};
+    console.log(`Sending message: ${eventData.body}`);
+    await client.send(eventData);
+  }
+
+  await client.close();
+}
+
+main().catch(err => {
+  console.log('Error occurred: ', err);
+});
