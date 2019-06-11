13061a5dbb80251e8c6ff56eeb1d81129aa915df
borysl <borys.lebeda@gmail.com>
2019-06-11 20:16:17 +0300

    Replace deflate again with gzip
    take data folder from config

--- receive.js
+++ receive.js
@@ -3,7 +3,7 @@ const { EventHubClient, delay } = require('@azure/event-hubs');
 // Define connection string and related Event Hubs entity name here
 const { connectionString, eventHubsName } = require('./config.json');
 
-const { inflateSync } = require('zlib');
+const { gunzipSync } = require('zlib');
 
 async function main() {
   const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName);
@@ -11,7 +11,7 @@ async function main() {
   const firstPartitionId = allPartitionIds[0];
 
   const receiveHandler = client.receive(firstPartitionId, eventData => {
-    let body = inflateSync(eventData.body);
+    let body = gunzipSync(eventData.body);
     console.log(`Received message length ${body.length} from partition ${firstPartitionId}`);
   }, error => {
     console.log('Error when receiving message: ', error);
--- send.js
+++ send.js
@@ -1,21 +1,20 @@
 const { EventHubClient } = require('@azure/event-hubs');
 
-const { readdirSync, readFileSync } = require('fs');
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
-const { deflateSync } = require('zlib');
+const { gzipSync } = require('zlib');
 
 // Define connection string and the name of the Event Hub
-const { connectionString, eventHubsName } = require('./config.json');
+const { connectionString, eventHubsName, dir } = require('./config.json');
 
 async function main() {
   const client = EventHubClient.createFromConnectionString(connectionString, eventHubsName);
 
-  const dir = 'data';
   const files = readdirSync(dir).map(name => join(dir, name));
 
   for (let file of files) {
-    var zipline = deflateSync(readFileSync(file));
+    var zipline = gzipSync(readFileSync(file));
     const eventData = { body: zipline };
     console.log(`Sending message from file: ${file}`);
     await client.send(eventData);
